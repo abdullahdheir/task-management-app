3f9a7d6b8df8467c7e316f8c427a41a39ba1ae76
Abdullah Dheir <tareqm577@gmail.com>
2026-07-09 21:45:27 +0300

    Add HasFactory trait to TaskAttachment model
    - Added HasFactory trait to TaskAttachment model
    - This allows TaskAttachment::factory() to work in the seeder

--- app/Models/TaskAttachment.php
+++ app/Models/TaskAttachment.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TaskAttachment extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['task_id', 'user_id', 'filename', 'path', 'mime_type', 'size'];
 
     public function task(): BelongsTo
